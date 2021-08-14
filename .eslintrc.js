58b2f92e1b220e4c0e698563f639ef3e890c1c53
panczarny <czarnota.michael94+github@gmail.com>
2021-08-14 15:45:23 +0200

    Added eslint&&prettier

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'prettier', '@typescript-eslint/eslint-plugin'],
+  extends: ['react-app', 'airbnb-typescript', 'prettier'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+      },
+    ],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+  },
+  ignorePatterns: ['.eslintrc.js'],
+};
